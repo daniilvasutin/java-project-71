735af5201107743515d2846112b574f5aafb7ee0
daniilvasutin <daniilvasutin@mail.ru>
2023-02-12 14:12:27 +0700

    3 step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,45 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "checksum 4.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer>{
+
+
+//    @Parameters(index = "0", description = "The file whose checksum to calculate.")
+//    private File file;
+//
+//    @Option(names = {"-a", "--algorithm"}, description = "MD5, SHA-1, SHA-256, ...")
+//    private String algorithm = "SHA-256";
+//
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+//        byte[] fileContents = Files.readAllBytes(file.toPath());
+//        byte[] digest = MessageDigest.getInstance(algorithm).digest(fileContents);
+//        System.out.printf("%0" + (digest.length * 2) + "x%n", new BigInteger(1, digest));
+        return 0;
     }
 
+    // this example implements Callable, so parsing, error handling and handling user
+    // requests for usage help or version help can be done with one line of code.
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
 }
